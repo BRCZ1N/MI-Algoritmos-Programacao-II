75badc00bb32d7196b332bdf3da7a7e73229c2ca
BRCZ1N <brunocamposo.r@gmail.com>
2022-05-24 12:57:09 -0300

    Organizando em pacotes

--- JavaFX_PBL_3/src/com/application/controller/SampleController.java
+++ JavaFX_PBL_3/src/com/application/controller/SampleController.java
@@ -1,4 +1,4 @@
-package application;
+package com.application.controller;
 
 public class SampleController {
 	
--- JavaFX_PBL_3/src/module-info.java
+++ JavaFX_PBL_3/src/module-info.java
@@ -2,5 +2,5 @@ module JavaFX_PBL_3 {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens com.application.main to javafx.graphics, javafx.fxml;
 }
