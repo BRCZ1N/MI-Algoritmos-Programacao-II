cf8500c8cab390f43bcd0d4e21532629fb9d8534
BRCZ1N <brunocamposo.r@gmail.com>
2022-05-26 12:57:50 -0300

    TestAlteração

--- JavaFX_PBL_3/src/applicationmain/Main.java
+++ JavaFX_PBL_3/src/applicationmain/Main.java
@@ -3,7 +3,7 @@ package applicationmain;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,11 +11,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			VBox root = (VBox)FXMLLoader.load(getClass().getResource("/applicationviewfxml/Sample.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/applicationviewfxml/PaginaPrincipal.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("/applicationviewcss/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setResizable(false);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- JavaFX_PBL_3/src/module-info.java
+++ JavaFX_PBL_3/src/module-info.java
@@ -1,6 +1,7 @@
 module JavaFX_PBL_3 {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens applicationcontroller to javafx.fxml;
 	opens applicationmain to javafx.graphics;
