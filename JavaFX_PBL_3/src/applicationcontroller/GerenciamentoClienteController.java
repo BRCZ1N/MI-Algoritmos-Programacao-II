8563045f03b6cccb26f0c21d5fec5dce286a06b0
BRCZ1N <brunocamposo.r@gmail.com>
2022-05-30 19:43:28 -0300

    Pagina de gerenciamento e login

---
+++ JavaFX_PBL_3/src/applicationcontroller/GerenciamentoClienteController.java
@@ -0,0 +1,23 @@
+package applicationcontroller;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TableView;
+
+public class GerenciamentoClienteController {
+	@FXML
+	private Button EditFuncionarios;
+	@FXML
+	private Button VoltarMenu;
+	@FXML
+	private Button DeleteFuncionarios;
+	@FXML
+	private Button RelatoryGenerate;
+	@FXML
+	private Button NewFuncionario;
+	@FXML
+	private TableView TableFuncionario;
+
+}
--- JavaFX_PBL_3/src/applicationmain/Main.java
+++ JavaFX_PBL_3/src/applicationmain/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/applicationviewcssfxml/PaginaPrincipal.fxml"));
 			Scene scene = new Scene(root,700,400);
-			scene.getStylesheets().add(getClass().getResource("/applicationviewcssfxml/PaginaPrincipal.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/applicationviewcssfxml/EfeitosAnimacoes.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			primaryStage.setResizable(false);
