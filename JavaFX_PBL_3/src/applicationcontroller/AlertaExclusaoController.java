4ff4f5775c7d22aed6466cb546bcb77cabfc3673
BRCZ1N <brunocamposo.r@gmail.com>
2022-05-31 18:45:10 -0300

    Alterando FXML e Controllers

---
+++ JavaFX_PBL_3/src/applicationcontroller/AlertaExclusaoController.java
@@ -0,0 +1,17 @@
+package applicationcontroller;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.image.ImageView;
+
+public class AlertaExclusaoController {
+	@FXML
+	private Button botaoExcluir;
+	@FXML
+	private Button botaoCancelarExcluir;
+	@FXML
+	private ImageView botaoCancelarExclusao;
+
+}
