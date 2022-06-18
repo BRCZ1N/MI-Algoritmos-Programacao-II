66ced332cc11e9a429dfb6ad2f8b2c547c287d86
xXgueloseXx <88115079+xXgueloseXx@users.noreply.github.com>
2022-06-17 23:57:59 -0300

    Update FormularioClientesController.java

--- JavaFX_PBL_3/src/applicationcontroller/FormularioClientesController.java
+++ JavaFX_PBL_3/src/applicationcontroller/FormularioClientesController.java
@@ -6,16 +6,27 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 import applicationexeceptions.CpfJaExisteException;
 import applicationexeceptions.LoginExistenteException;
 import applicationmain.Main;
 import applicationmodel.Clientes;
+import applicationmodel.Pratos;
+import applicationmodel.Vendas;
 import applicationmodeldao.DaoClientes;
+import applicationmodeldao.DaoPratos;
+import applicationmodeldao.DaoVendas;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
 public class FormularioClientesController implements Initializable {
@@ -33,6 +44,33 @@ public class FormularioClientesController implements Initializable {
 	private Button salvarClienteBotao;
 
 	private ArrayList<String> historicoCompras;
+	
+	@FXML
+	private TableColumn<Vendas, String> columnCarrinhoPratoId;
+	@FXML
+	private TableColumn<Vendas, Double> columnCarrinhoPratoValor;
+	@FXML
+	private TableColumn<Vendas, Calendar> columnCarrinhoVendaDHorario;
+	@FXML
+	private TableColumn<Vendas, String> columnSistemaVendaId;
+	@FXML
+	private TableColumn<Vendas, Double> columnSistemaVendaValor;
+	@FXML
+	private TableColumn<Vendas, Calendar> columnSistemaVendaDHorario;
+	
+	@FXML
+	private TableView<Vendas> tabelaVendas;
+	@FXML
+	private TableView<Vendas> tabelaCompraCliente;
+
+	private ObservableList<Vendas> observableVendaSistema;
+
+	private ObservableList<Vendas> observableVendaCarrinho;
+
+	private ArrayList<Vendas> listaVendasCarrinho = new ArrayList<Vendas>();
+
+
+	
 
 	private static Clientes clienteAtual;
 
@@ -110,6 +148,47 @@ public class FormularioClientesController implements Initializable {
 		Scene scene = new Scene(root);
 
 		return scene;
+
+	}
+	@FXML
+	void acaoAdicionarItemVenda(ActionEvent event) {
+
+		listaVendasCarrinho.addAll(tabelaVendas.getSelectionModel().getSelectedItems());
+		refreshCarrinho();
+
+	}
+
+	@FXML
+	void acaoRemoverItemVenda(ActionEvent event) {
+
+		for (Vendas pratoExcluir : tabelaCompraCliente.getSelectionModel().getSelectedItems()) {
+
+			listaVendasCarrinho.remove(pratoExcluir);
+
+		}
+		refreshCarrinho();
+
+	}
+
+	public void refreshCarrinho() {
+
+		observableVendaCarrinho = FXCollections.observableArrayList(listaVendasCarrinho);
+		tabelaCompraCliente.setItems(observableVendaCarrinho);
+
+		columnCarrinhoPratoId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		columnCarrinhoPratoValor.setCellValueFactory(new PropertyValueFactory<>("precoTotal"));
+		columnCarrinhoVendaDHorario.setCellValueFactory(new PropertyValueFactory<>("diaHorario"));
+
+	}
+
+	public void refreshSistema() {
+
+		observableVendaSistema = FXCollections.observableArrayList(DaoVendas.getListaVendas());
+		tabelaVendas.setItems(observableVendaSistema);
+
+		columnSistemaVendaId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		columnSistemaVendaValor.setCellValueFactory(new PropertyValueFactory<>("nome"));
+		columnSistemaVendaDHorario.setCellValueFactory(new PropertyValueFactory<>("diaHorario"));
 
 	}
 }
